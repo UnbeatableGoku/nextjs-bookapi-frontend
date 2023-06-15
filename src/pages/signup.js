e0a24b0d363d8e433b887743f24bcdb552b6a0b5
Prathmesh Pandya <pandyaprathmesh360@gmail.com>
2023-06-15 11:01:13 +0530

    add form css

--- src/pages/signup.js
+++ src/pages/signup.js
@@ -14,21 +14,23 @@ export default function Signup() {
   return (
     <>
       {status === 'unauthenticated' && (
-        <div className='flex h-screen bg-slate-100  '>
-          <div className='flex flex-col   md:flex-row-reverse  w-full max-w-xl m-auto items-center shadow-md shadow-slate-900 bg-white rounded-lg  max-sm:p-0'>
+        <div className='flex h-screen bg-slate-100  p-10'>
+          <div className='flex flex-col  md:flex-row  w-full max-w-xl m-auto items-center shadow-md border  shadow-black bg-fuchsia-50 rounded-lg  p-12 sm:p-10 md:p-0'>
             <div className='w-full mx-auto'>
               <img
                 className='mx-auto  max-sm:hidden max-md:hidden'
                 src='https://images.unsplash.com/photo-1535905557558-afc4877a26fc?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=387&q=80'
               />
             </div>
-            <div className='px-3 text-lg'>
+            <div className='bg-img'></div>
+
+            <div className='px-3 text-lg z-50'>
               <div className='text-4xl pb-4'>SIGNUP</div>
               <form onSubmit={handleSubmit(handleFormSubmit)}>
                 <div className='w-full border-b-2 border-b-slate-700 '>
                   <input
                     placeholder='Email'
-                    className='pt-4'
+                    className='pt-4 bg-transparent  placeholder:text-black'
                     {...register('email')}
                     required
                   />
@@ -36,7 +38,7 @@ export default function Signup() {
                 <div className='w-full border-b-2 border-b-slate-700 '>
                   <input
                     placeholder='Username'
-                    className='pt-4'
+                    className='pt-4 bg-transparent  placeholder:text-black'
                     {...register('username')}
                     required
                   />
@@ -44,7 +46,7 @@ export default function Signup() {
                 <div className='w-full  border-b-2 border-b-slate-700 '>
                   <input
                     placeholder='Password'
-                    className='pt-4'
+                    className='pt-4 bg-transparent  placeholder:text-black'
                     type='password'
                     {...register('password')}
                   />
